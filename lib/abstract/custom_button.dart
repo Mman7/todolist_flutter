6c4f30ff3d9d78d3cbb5cab0b189b22983db68a8
Mackenzie <youzai0427@gmail.com>
2021-12-19 17:04:11 +0800

    ignore some suggestion

--- lib/abstract/custom_button.dart
+++ lib/abstract/custom_button.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class CustomButton extends StatelessWidget {
   CustomButton({Key? key, required this.callback, required this.iconData})
       : super(key: key);
+  // ignore: prefer_typing_uninitialized_variables
   final callback;
   IconData iconData;
   @override
