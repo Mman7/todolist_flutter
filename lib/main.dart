66d3567938dabcd79b7ea81964f573811026c92e
Mackenzie <youzai0427@gmail.com>
2022-01-11 20:02:01 +0800

    abstract theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:hexcolor/hexcolor.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'abstract/theme/theme.dart';
 import 'abstract/widget/custom_pop_up_inside_layout.dart';
 import 'abstract/widget/custom_button.dart';
 import 'abstract/widget/todo_item.dart';
 import 'abstract/todo_controller.dart';
 import 'package:provider/provider.dart';
 import 'abstract/providers/theme_provider.dart';
-import 'package:tinycolor/tinycolor.dart';
 
 void main() {
   runApp(MultiProvider(
@@ -24,56 +23,8 @@ class MyApp extends StatelessWidget {
       home: const TodoList(),
       title: 'Simple Todo',
       themeMode: context.watch<ThemeProvider>().themeMode,
-      darkTheme: ThemeData(
-          floatingActionButtonTheme: FloatingActionButtonThemeData(
-              foregroundColor: Theme.of(context).secondaryHeaderColor,
-              backgroundColor: Theme.of(context).primaryColor),
-          dialogTheme: DialogTheme(
-              contentTextStyle: const TextStyle(color: Colors.white),
-              backgroundColor: const Color.fromARGB(255, 39, 39, 39),
-              titleTextStyle:
-                  TextStyle(color: Theme.of(context).colorScheme.secondary)),
-          popupMenuTheme: PopupMenuThemeData(
-            color: Colors.black.withAlpha(1000),
-          ),
-          appBarTheme: AppBarTheme(
-              backgroundColor: Theme.of(context).primaryColor.darken(45)),
-          textTheme: const TextTheme(
-            bodyText1: TextStyle(color: Colors.white, fontSize: 20),
-          ),
-          primaryColor: Colors.blue,
-          bottomNavigationBarTheme: BottomNavigationBarThemeData(
-              backgroundColor: Theme.of(context).primaryColor.darken(45),
-              selectedItemColor: Colors.blue,
-              unselectedItemColor: Colors.grey.withOpacity(0.5)),
-          backgroundColor: Colors.white.withOpacity(0.15)),
-      theme: ThemeData(
-          textButtonTheme: TextButtonThemeData(
-              style: ButtonStyle(
-                  foregroundColor: MaterialStateProperty.all(
-                      Theme.of(context).bottomAppBarColor),
-                  backgroundColor: MaterialStateProperty.all(
-                      Theme.of(context).primaryColor))),
-          primaryColor: Colors.blue,
-          backgroundColor: HexColor('#E5EEF5'),
-          canvasColor: HexColor('#ffffff').withOpacity(0.35),
-          dialogTheme: DialogTheme(
-              backgroundColor: Theme.of(context).bottomAppBarColor,
-              titleTextStyle: TextStyle(color: Theme.of(context).primaryColor)),
-          popupMenuTheme: PopupMenuThemeData(
-              color: Theme.of(context).bottomAppBarColor,
-              shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(5)))),
-          appBarTheme: AppBarTheme(
-              foregroundColor: Colors.white,
-              backgroundColor: Theme.of(context).primaryColor),
-          bottomNavigationBarTheme: BottomNavigationBarThemeData(
-            backgroundColor: Colors.blue,
-            selectedItemColor: Colors.white,
-          ),
-          textTheme: const TextTheme(
-            bodyText1: TextStyle(color: Colors.white, fontSize: 20),
-          )),
+      darkTheme: dark_theme(context),
+      theme: lightTheme(context),
     );
   }
 }
@@ -255,7 +206,7 @@ class _TodoListState extends State<TodoList> {
           _inputText = value;
         });
         TodoController()
-            .addTask(todoList: _todoTask, value: _inputText)
+            .addTask(context: context, todoList: _todoTask, value: _inputText)
             .then((newTodoList) => setState(() => _todoTask = newTodoList));
         _textFieldController.text = '';
       },
