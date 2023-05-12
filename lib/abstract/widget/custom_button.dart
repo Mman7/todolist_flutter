d91105f1804701ba2c28b26b5378ba8fef10ceeb
MackMan <youzai0427@gmail.com>
2023-05-12 19:42:23 +0800

    fix code

--- lib/abstract/widget/custom_button.dart
+++ lib/abstract/widget/custom_button.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
-// ignore: must_be_immutable
 class CustomButton extends StatelessWidget {
-  CustomButton({Key? key, required this.callback, required this.iconData})
+  const CustomButton({Key? key, required this.callback, required this.iconData})
       : super(key: key);
   // ignore: prefer_typing_uninitialized_variables
   final callback;
-  IconData iconData;
+  final IconData iconData;
   @override
   Widget build(BuildContext context) {
     return IconButton(
--- lib/abstract/widget/custom_pop_up_inside_layout.dart
+++ lib/abstract/widget/custom_pop_up_inside_layout.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-// ignore: must_be_immutable
 class CustomPopUpInside extends StatelessWidget {
-  CustomPopUpInside({Key? key, required this.text, required this.iconData})
+  const CustomPopUpInside(
+      {Key? key, required this.text, required this.iconData})
       : super(key: key);
 
-  IconData iconData;
-  String text;
+  final IconData iconData;
+  final String text;
 
   @override
   Widget build(BuildContext context) {
