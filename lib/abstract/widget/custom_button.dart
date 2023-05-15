9b50f965bc8163974da861a2fedb11e34f3ce53f
MackMan <youzai0427@gmail.com>
2023-05-16 03:17:31 +0800

    update fix code

--- lib/abstract/widget/custom_button.dart
+++ lib/abstract/widget/custom_button.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 class CustomButton extends StatelessWidget {
   const CustomButton({Key? key, required this.callback, required this.iconData})
       : super(key: key);
-  // ignore: prefer_typing_uninitialized_variables
-  final callback;
+  final dynamic callback;
   final IconData iconData;
   @override
   Widget build(BuildContext context) {
