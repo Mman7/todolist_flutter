6bab08ef6d016ea31ff19aad563ad298df555165
MackMan <youzai0427@gmail.com>
2023-05-17 01:10:38 +0800

    seperate code

---
+++ lib/abstract/widget/custom_floating_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+Widget customFloatingButton({required BuildContext context, required onPress}) {
+  return Container(
+    decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+              color: HexColor('#0085FF').withOpacity(0.65),
+              spreadRadius: 0.25,
+              blurRadius: 20,
+              offset: const Offset(0, 0))
+        ],
+        color: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.circular(50)),
+    child: FloatingActionButton(
+      onPressed: onPress,
+      child: const Icon(
+        Icons.add,
+      ),
+    ),
+  );
+}
