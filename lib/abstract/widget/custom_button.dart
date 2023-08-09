3c98d35519b850de097e7a22d09638147eeb6356
MackMan <youzai0427@gmail.com>
2023-08-09 23:03:53 +0800

    fix gesture double tap problem

--- lib/abstract/widget/custom_button.dart
+++ lib/abstract/widget/custom_button.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/data_provider.dart';
 
-class CustomButton extends StatelessWidget {
-  const CustomButton({Key? key, required this.callback, required this.iconData})
+class CustomButton extends StatefulWidget {
+  const CustomButton({Key? key, required this.iconData, required this.callback})
       : super(key: key);
-  final dynamic callback;
   final IconData iconData;
+  final Function callback;
+
+  @override
+  State<CustomButton> createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<CustomButton> {
+  Offset buttonPosition = const Offset(0, 0);
+  late DataProvider dataContext;
+  @override
+  void initState() {
+    super.initState();
+    dataContext = context.read<DataProvider>();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-        color: Colors.white, onPressed: callback, icon: Icon(iconData));
+    return Listener(
+      onPointerDown: (e) {
+        setState(() {
+          buttonPosition = e.position;
+        });
+      },
+      onPointerUp: (e) {
+        if (buttonPosition != e.position) return;
+        dataContext.updatePos(e.position);
+        widget.callback();
+      },
+      child: IconButton(
+          color: Colors.white, onPressed: () {}, icon: Icon(widget.iconData)),
+    );
   }
 }
