ed1575756feb37a4371af8e4a5277114ae6e83d6
MackMan <youzai0427@gmail.com>
2023-10-13 03:51:47 +0800

    @update theme colour

--- lib/abstract/theme/theme.dart
+++ lib/abstract/theme/theme.dart
@@ -3,16 +3,23 @@ import 'package:hexcolor/hexcolor.dart';
 
 ThemeData darkTheme(BuildContext context) {
   return ThemeData(
+    textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+      backgroundColor: HexColor('#0057FF'),
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10))),
+    )),
     canvasColor: Colors.white10,
     shadowColor: HexColor('#1C92FF').withAlpha(125),
     floatingActionButtonTheme: FloatingActionButtonThemeData(
         foregroundColor: Theme.of(context).secondaryHeaderColor,
         backgroundColor: HexColor('#00D1FF')),
     dialogTheme: DialogTheme(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(17)),
         contentTextStyle: const TextStyle(color: Colors.white),
         backgroundColor: const Color.fromARGB(255, 39, 39, 39),
-        titleTextStyle:
-            TextStyle(color: Theme.of(context).colorScheme.secondary)),
+        titleTextStyle: const TextStyle(
+            color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20)),
     appBarTheme: AppBarTheme(backgroundColor: HexColor('#040934')),
     textTheme: const TextTheme(
       bodyLarge: TextStyle(color: Colors.white, fontSize: 20),
@@ -22,7 +29,7 @@ ThemeData darkTheme(BuildContext context) {
       background: HexColor(('#110630')),
       brightness: Brightness.dark,
     ),
-    primaryColor: Colors.blue,
+    primaryColor: HexColor('#0057FF'),
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
         backgroundColor: HexColor('#040934'),
         selectedItemColor: Colors.blue,
